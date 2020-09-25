0b1d4eaa80f2ac5fefca7b50aa26a0572dee626e
eneko1225 <59622429+eneko1225@users.noreply.github.com>
2020-09-25 12:50:03 +0200

    Add files via upload

---
+++ HolaMundoApplication/src/View/ViewImplementation.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+/**
+ * View implemementation to show greeting in a tect type
+ * @author Eneko
+ */
+public class ViewImplementation {
+    
+    /**
+     *  Shoes  greetin UI
+     * @param greeting  The geeting it`s self
+     */
+    public void showGreeting(String greeting){
+        System.out.println(greeting);
+    }
+}
